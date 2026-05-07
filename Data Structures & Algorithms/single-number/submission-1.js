35ca0e937980b8aaec1e4fd642cebd2732ac433a
Yassir Ait Ammi <151558289+yassir-ait-ammi@users.noreply.github.com>
2026-05-07 16:02:39 +0100

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let result = 0;
+
+        for (let num of nums) {
+            result ^= num;
+        }
+        return result;
+    };
+}
